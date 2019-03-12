87019d9160f2c47c7625b36e2c8f464b7c45490d
Moin Uddin <U_Moin75@yahoo.com>
2019-03-12 18:19:01 -0400

    Add pg

--- knexfile.js
+++ knexfile.js
@@ -33,18 +33,19 @@ module.exports = {
   // },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: "pg",
+    connection: 'postgres://rucvhahrxkgtzo:2319d4d8f91ed58ecb9def5ef8d9f2fbffd7343b845589be039506e76a046f16@ec2-23-23-173-30.compute-1.amazonaws.com:5432/d3f370u7m5gug5',
     pool: {
       min: 2,
       max: 10
     },
+    useNullAsDefault: true,
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: "knex_migrations",
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
     }
   }
 
